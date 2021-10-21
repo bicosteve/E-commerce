58ad825170125744fbc80fff4d7a4c3d8ea21139
bicosteve <bicosteve4@gmail.com>
2021-10-21 15:11:25 +0300

    Added database configuration

---
+++ classes/Database.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once 'config/config.php';
+
+class Database {
+  private $host = DB_HOST;
+  private $dbname = DB_NAME;
+  private $username = DB_USER;
+  private $password = DB_PASSWORD;
+
+  private $connection;
+  private $db_connection = false;
+
+  private $options = [
+  PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
+  PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC,
+  PDO::ATTR_EMULATE_PREPARES=>false
+  ];
+
+  function __construct
+  {
+    $dsn = "mysql:host=$this->db_host;dbname=$this->db_name";
+    try{
+      $this->connection = new PDO($dsn,$this->username,$this->password,$this->options);
+      $this->db_connection = true;
+    }catch(PDOException $er){
+      echo 'Connection failed ...'.$er->getMessage();
+      $this->db_connection = false;
+    }
+  }
+}
